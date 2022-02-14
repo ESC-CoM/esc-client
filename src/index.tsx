1a5fc92df10f4ebcea563b42ca6516b4d83069c6
동동이 <drdragon777@naver.com>
2022-02-14 19:38:13 +0900

    setting : Add responsive design scss setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import './styles/reset.scss';
 
 ReactDOM.render(
   <Provider store={store}>
