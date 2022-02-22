d7a72d8dfb2fee09bc855788545b1ad172c9730e
juyeong-chungbuk <happyu9986@gmail.com>
2022-02-23 02:19:48 +0900

    chore : Edit yup message

--- src/components/Join/BasicJoin/yup.tsx
+++ src/components/Join/BasicJoin/yup.tsx
@@ -4,16 +4,16 @@ const JoinSchema = yup.object({
   email: yup
     .string()
     .email('올바른 이메일 형식을 입력해주세요.')
-    .required('이메일을 입력해주세요.'),
+    .required('필수 입력사항입니다.'),
   password: yup
     .string()
     .min(8, '비밀번호는 영문, 숫자 포함 최소 8자입니다.')
     .max(30, '비밀번호는 영문, 숫자 포함 최대 30자입니다.')
-    .required('비밀번호를 입력해주세요.'),
+    .required('필수 입력사항입니다.'),
   passwordConfirm: yup
     .string()
     .oneOf([yup.ref('password'), null], '비밀번호가 일치하지 않습니다.'),
-  sex: yup.string().required('성별을 선택해주세요.'),
+  sex: yup.string().required('필수 입력사항입니다.'),
   year: yup.string().required('생년월일을 선택해주세요.'),
   month: yup.string().required('생년월일을 선택해주세요.'),
   day: yup.string().required('생년월일을 선택해주세요.'),
