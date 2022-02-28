49d0c79d094fb6d9ec29f1ab5ecefdbd3886016a
동동이 <drdragon777@naver.com>
2022-03-01 08:46:30 +0900

    feat : Add InfiniteScroll Layout & custom hooks

--- src/__mocks__/meetingBoardMocks.ts
+++ src/__mocks__/meetingBoardMocks.ts
@@ -53,60 +53,6 @@ const meetingBoardMocks = [
     gender: '여자',
     num: 2,
   },
-  {
-    kind: '과팅',
-    college: '전자정보대학',
-    gender: '남자',
-    num: 3,
-  },
-  {
-    kind: '과팅',
-    college: '공과대학',
-    gender: '남자',
-    num: 5,
-  },
-  {
-    kind: '과팅',
-    college: '경영대학',
-    gender: '여자',
-    num: 2,
-  },
-  {
-    kind: '과팅',
-    college: '전자정보대학',
-    gender: '남자',
-    num: 3,
-  },
-  {
-    kind: '과팅',
-    college: '공과대학',
-    gender: '남자',
-    num: 5,
-  },
-  {
-    kind: '과팅',
-    college: '경영대학',
-    gender: '여자',
-    num: 2,
-  },
-  {
-    kind: '과팅',
-    college: '전자정보대학',
-    gender: '남자',
-    num: 3,
-  },
-  {
-    kind: '과팅',
-    college: '공과대학',
-    gender: '남자',
-    num: 5,
-  },
-  {
-    kind: '과팅',
-    college: '경영대학',
-    gender: '여자',
-    num: 2,
-  },
 ];
 
 export { meetingBoardMocks };
