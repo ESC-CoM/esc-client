9e5414e1d4bfddb5cca1b5017093bb2972ddfed3
동동이 <drdragon777@naver.com>
2022-03-01 02:21:36 +0900

    feat : Add meetingBoardMocks

---
+++ src/__mocks__/meetingBoardMocks.ts
@@ -0,0 +1,112 @@
+const meetingBoardMocks = [
+  {
+    kind: '과팅',
+    college: '전자정보대학',
+    gender: '남자',
+    num: 3,
+  },
+  {
+    kind: '과팅',
+    college: '공과대학',
+    gender: '남자',
+    num: 5,
+  },
+  {
+    kind: '과팅',
+    college: '경영대학',
+    gender: '여자',
+    num: 2,
+  },
+  {
+    kind: '과팅',
+    college: '전자정보대학',
+    gender: '남자',
+    num: 3,
+  },
+  {
+    kind: '과팅',
+    college: '공과대학',
+    gender: '남자',
+    num: 5,
+  },
+  {
+    kind: '과팅',
+    college: '경영대학',
+    gender: '여자',
+    num: 2,
+  },
+  {
+    kind: '과팅',
+    college: '전자정보대학',
+    gender: '남자',
+    num: 3,
+  },
+  {
+    kind: '과팅',
+    college: '공과대학',
+    gender: '남자',
+    num: 5,
+  },
+  {
+    kind: '과팅',
+    college: '경영대학',
+    gender: '여자',
+    num: 2,
+  },
+  {
+    kind: '과팅',
+    college: '전자정보대학',
+    gender: '남자',
+    num: 3,
+  },
+  {
+    kind: '과팅',
+    college: '공과대학',
+    gender: '남자',
+    num: 5,
+  },
+  {
+    kind: '과팅',
+    college: '경영대학',
+    gender: '여자',
+    num: 2,
+  },
+  {
+    kind: '과팅',
+    college: '전자정보대학',
+    gender: '남자',
+    num: 3,
+  },
+  {
+    kind: '과팅',
+    college: '공과대학',
+    gender: '남자',
+    num: 5,
+  },
+  {
+    kind: '과팅',
+    college: '경영대학',
+    gender: '여자',
+    num: 2,
+  },
+  {
+    kind: '과팅',
+    college: '전자정보대학',
+    gender: '남자',
+    num: 3,
+  },
+  {
+    kind: '과팅',
+    college: '공과대학',
+    gender: '남자',
+    num: 5,
+  },
+  {
+    kind: '과팅',
+    college: '경영대학',
+    gender: '여자',
+    num: 2,
+  },
+];
+
+export { meetingBoardMocks };
