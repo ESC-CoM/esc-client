71b76d465461c16f3403d758ea02517f73ae64a6
juyeong-chungbuk <happyu9986@gmail.com>
2022-03-01 06:19:19 +0900

    feat : Export ToS component

--- src/components/Join/index.ts
+++ src/components/Join/index.ts
@@ -1,3 +1,4 @@
 export { default as BasicInfo } from './BasicJoin';
 export { default as Personality } from './PersonalityJoin';
 export { default as PhoneVerify } from './PhoneVerify';
+export { default as ToS } from './ToS';
