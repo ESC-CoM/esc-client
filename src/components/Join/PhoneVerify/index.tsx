5fa672141defa32a0ccd98484e8eff2d7a7c5483
juyeong-chungbuk <happyu9986@gmail.com>
2022-03-06 19:03:00 +0900

    feat : Combine PhoneAuth with BasicInfo component

--- src/components/Join/PhoneVerify/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function PhoneVerify() {
-  return (
-    <div>
-      <h1>휴대폰 번호를 인증해주세요.</h1>
-    </div>
-  );
-}
--- src/components/Join/index.ts
+++ src/components/Join/index.ts
@@ -1,3 +1,2 @@
 export { default as BasicInfo } from './BasicJoin';
 export { default as Personality } from './PersonalityJoin';
-export { default as PhoneVerify } from './PhoneVerify';
