1b03f176a43020a3a4f0be5c2edc4db0b9f98a97
juyeong-chungbuk <happyu9986@gmail.com>
2022-03-13 02:23:21 +0900

    feat, ui&ux : Implement the agree to all function of terms

--- src/components/Join/Terms/index.tsx
+++ src/components/Join/Terms/index.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import TermsSchema from './yup';
 import { FiCheck, FiChevronRight } from 'react-icons/fi';
 import { useEffect, useState } from 'react';
+import { watch } from 'fs';
 
 interface Terms {
   // Todo: types 파일에 옮기기
@@ -20,10 +21,10 @@ type State = {
 export default function Terms({ onState, onClickToggleModal }: State) {
   const {
     control,
-    register,
+    watch,
     handleSubmit,
     reset,
-    getValues,
+    // getValues,
     setValue,
     formState: { errors },
   } = useForm<Terms>({
@@ -44,37 +45,35 @@ export default function Terms({ onState, onClickToggleModal }: State) {
       title: '개인정보 수집 및 이용 동의(필수)',
       url: 'http://2',
     },
+    // {
+    //   title: '본인 확인 서비스 고유식별정보 처리 동의사항(선택)',
+    //   url: 'http://3',
+    // },
   ];
-  // const personalAgree = register('personalAgree', { required: true });
+
+  const termItems = watch();
+  const itemsLen = Object.keys(termItems).length;
+  const [allChecked, setAllChecked] = useState<boolean>(false);
 
   const termsValue = (idx: number) => (!idx ? 'personalAgree' : 'acceptAgree');
-  // const termsType: Terms = { personalAgree: false, acceptAgree: false };
-
-  const [allChecked, setAllChecked] = useState<boolean>(false);
-  const [itemsLen, setItemsLen] = useState(2); // 전체 아이템 길이
-  // const [personalAgree, acceptAgree] = watch(['personalAgree', 'acceptAgree']);
-  const onHandleAllCheck = () => {
-    setAllChecked(!allChecked);
+  const onHandleAllCheck = (check: boolean) => {
+    setAllChecked(!check);
     reset({
-      personalAgree: allChecked,
-      acceptAgree: allChecked,
+      personalAgree: !check,
+      acceptAgree: !check,
     });
-    // }
   };
-  const [trueLen, setTrueLen] = useState(0); // true인것의 길이
 
   useEffect(() => {
-    const values = getValues();
-    console.log(values);
-    for (const value in values) {
-      const state = values[value];
+    let trueLen = 0;
+    for (const item in termItems) {
+      const state = termItems[item];
       if (state) {
-        console.log(1);
-        setTrueLen(trueLen + 1);
-      } else setTrueLen(trueLen - 1);
+        trueLen++;
+      }
     }
-    itemsLen !== trueLen ? setAllChecked(false) : setAllChecked(true);
-  }, [allChecked]);
+    itemsLen === trueLen ? setAllChecked(true) : setAllChecked(false);
+  }, [watch()]);
 
   const onSubmit = (data: Terms) => console.log(data);
 
@@ -85,91 +84,53 @@ export default function Terms({ onState, onClickToggleModal }: State) {
         onSubmit={handleSubmit(onSubmit)}
       >
         <h1>서비스 이용을 위해 동의가 필요해요</h1>
-        <section className={style.item} onClick={() => onHandleAllCheck()}>
-          <FiCheck
-            className={style.checkbox}
-            size="1.2em"
-            color={!allChecked ? 'gray' : '#ff5c66'}
-          />
-          {itemsLen}
-          {trueLen}
-          <em>모두 동의하기</em>
-        </section>
-        {terms.map((term, idx) => {
-          const { title, url } = term;
-          const named = termsValue(idx);
-          return (
-            <section
-              className={style.item}
-              onClick={() => setValue(named, !getValues(named))}
-              key={idx}
-            >
-              <Controller
-                name={named}
-                control={control}
-                render={({ field: { onChange, onBlur } }) => (
-                  <FiCheck
-                    className={style.checkbox}
-                    size="1.2em"
-                    color={!getValues(named) ? 'gray' : '#ff5c66'}
-                    onChange={onChange}
-                    onBlur={onBlur}
-                  />
-                )}
-              />
-              <strong>{title}</strong>
-              <a href={url} className={style.content}>
-                <FiChevronRight />
-              </a>
-            </section>
-          );
-        })}
-        {/* <section
-          className={style.item}
-          onClick={() => setValue('personalAgree', !personalAgree)}
-        >
-          <Controller
-            name="personalAgree"
-            control={control}
-            render={({ field }) => (
-              <FiCheck
-                className={style.checkbox}
-                size="1.2em"
-                color={!personalAgree ? 'gray' : '#ff5c66'}
-                {...field}
-              />
-            )}
-          />
-          <strong>이용약관 동의</strong>
-          <span>(필수)</span>
-          <span className={style.content}>
-            <FiChevronRight />
-          </span>
-        </section>
-
         <section
-          className={style.item}
-          onClick={() => setValue('acceptAgree', !acceptAgree)}
+          className={style.allChecked}
+          onClick={() => onHandleAllCheck(allChecked)}
         >
-          <Controller
-            name="acceptAgree"
-            control={control}
-            render={({ field }) => (
-              <FiCheck
-                className={style.checkbox}
-                size="1.2em"
-                color={!acceptAgree ? 'gray' : '#ff5c66'}
-                {...field}
-              />
-            )}
-          />
-          <strong>개인정보 수집 및 이용 동의</strong>
-          <span>(필수)</span>
-          <span className={style.content}>
-            <FiChevronRight />
-          </span>
-        </section> */}
-
+          <FiCheck color={!allChecked ? 'gray' : '#ff5c66'} />
+          <em className={style.text}>모두 동의하기</em>
+        </section>
+        <ul className={style.terms_list}>
+          {terms.map((term, idx) => {
+            const { title, url } = term;
+            const named = termsValue(idx);
+            return (
+              <li
+                className={style.terms_bx}
+                onClick={() => setValue(named, !watch(named))}
+                key={idx}
+              >
+                <ul className={style.terms_item}>
+                  <li>
+                    {!watch(named)}
+                    <span className={style.checkbox}>
+                      <Controller
+                        name={named}
+                        control={control}
+                        render={({ field: { onChange, onBlur } }) => (
+                          <FiCheck
+                            color={!watch(named) ? 'gray' : '#ff5c66'}
+                            onChange={onChange}
+                            onBlur={onBlur}
+                          />
+                        )}
+                      />
+                    </span>
+                  </li>
+                  <li>
+                    <strong className={style.title}>{title}</strong>
+                  </li>
+                  <li>
+                    <a href={url} className={style.url}>
+                      <FiChevronRight />
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
         <span className={style.error}>
           {(errors.personalAgree || errors.acceptAgree) &&
             '필수 약관에 동의해주세요.'}
