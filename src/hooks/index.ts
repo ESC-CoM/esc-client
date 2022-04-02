6495661855607ad9247dda1a602cb77bd7013e07
동동이 <drdragon777@naver.com>
2022-04-03 05:32:20 +0900

    feat : Add tsconfig alias

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
+export { default as useWindowResize } from './useWindowResize';
 export { default as useIntersectObserver } from './useIntersectObserver';
 export { default as useExtractColleges } from './useExtractColleges';
