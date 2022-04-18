c5ffb307a43184f015c6dd03e941f88377d55aad
HyunoChoi <soonitoon@gmail.com>
2022-04-18 16:51:38 +0900

    feat : Make find page router (#53)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './styles/reset.scss';
 import MeetingBoardPage from './pages/MeetingBoard';
 import JoinRoute from './routes/JoinRoute';
 import LoginPage from './pages/loginPage';
+import AccountFinderRauter from './routes/AccountFinderRoute';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="*" element={<Navigate to="/home" />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/find/*" element={<AccountFinderRauter />} />
           <Route path="/home" element={<MeetingBoardPage />} />
           <Route path="/mymeeting" element={<h1>hello</h1>} />
           <Route path="/chat" element={<h1>hello</h1>} />
---
+++ src/routes/AccountFinderRoute.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import {
+  EmailConfirmPage,
+  EmailFinderPage,
+  PasswordConfirmPage,
+  PasswordFinderPage,
+} from 'src/pages/AccountFinderPages';
+
+function AccountFinderRauter() {
+  return (
+    <Routes>
+      <Route path="email" element={<EmailFinderPage />} />
+      <Route path="email/confirm" element={<EmailConfirmPage />} />
+      <Route path="password" element={<PasswordFinderPage />} />
+      <Route path="password/confirm" element={<PasswordConfirmPage />} />
+    </Routes>
+  );
+}
+
+export default AccountFinderRauter;
