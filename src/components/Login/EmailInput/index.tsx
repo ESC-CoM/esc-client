fc47f81a3d8f173cb59234ca65ac6e9bc5e02e1b
Bruney <drdragon777@naver.com>
2022-06-02 03:10:37 +0900

    [Feat] 헤더 컴포넌트 리팩토링 (#78)
    * feat : Add RegisterMeeting Header
    
    * feat : Add Header match logic
    
    * refactor : Header component in PageLayout
    
    * refactor : modify property that Header component exists or not
    
    * chore : delete relative
    
    * merge from dev to feat/i74
    
    * chore : reflect code review

--- src/components/Login/EmailInput/index.tsx
+++ src/components/Login/EmailInput/index.tsx
@@ -1,6 +1,6 @@
 import cx from 'classnames';
 import { ResetFieldButton } from '../../atoms';
-import $ from './style.module.scss';
+import styles from './style.module.scss';
 
 interface Props {
   register: () => void;
@@ -11,16 +11,16 @@ interface Props {
 
 function EmailInput({ register, resetField, className, isValueExists }: Props) {
   return (
-    <div className={cx($.container, className)}>
+    <div className={cx(styles.container, className)}>
       <input
-        className={$.input}
+        className={styles.input}
         placeholder="이메일"
         type="text"
         {...register()}
       />
       {isValueExists && (
         <ResetFieldButton
-          className={$.delete}
+          className={styles.delete}
           ariaLabel={'이메일 입력 초기화하기'}
           onClick={resetField}
         />
