27cf6026d6cdedbcabe9e87e3c05b7b193ce97fb
동동이 <drdragon777@naver.com>
2022-06-23 22:44:19 +0900

    feat : Add SelectFriend in MeetingApply

--- src/pages/Home/MeetingApply/index.tsx
+++ src/pages/Home/MeetingApply/index.tsx
@@ -1,14 +1,30 @@
 import { PageLayout } from 'src/components/Layout';
 import Search from 'src/components/Search';
+import { friendMocks } from 'src/__mocks__/friendMocks';
 import $ from './style.module.scss';
+import { useState } from 'react';
+import SelectFriend from 'src/components/SelectFriend';
 
 export default function MeetingApplyPage() {
+  const [addedList, setAddedList] = useState<number[]>([]);
   return (
     <PageLayout isNeedFooter={false} headerHeight={44}>
-      <main className={$['wrap']}>
+      <div className={$['top']}>
         <h2>친구 추가하기</h2>
         <Search />
-      </main>
+      </div>
+      <ul className={$['friends-list']}>
+        {friendMocks.map(({ src, name, isVertical }, id) => (
+          <li
+            key={src + name}
+            className={$['friend']}
+            onClick={() => setAddedList([...addedList, id])}
+          >
+            <SelectFriend {...{ src, name, isVertical }} />
+          </li>
+        ))}
+      </ul>
+      <ul className={$['added-friends']}></ul>
     </PageLayout>
   );
 }
