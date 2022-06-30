582b63f5c356083f2a5a6dcd39598b5e44ca20d0
동동이 <drdragon777@naver.com>
2022-07-01 03:05:43 +0900

    feat : Add ContentBox component

---
+++ src/components/ContentBox/index.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent, memo, useCallback, useRef, useState } from 'react';
+import useDebounceInput from 'src/hooks/useDebounceInput';
+import $ from './style.module.scss';
+
+interface Props {
+  title: string;
+  content: string;
+  isReadMode?: boolean;
+  contentTitle?: string;
+}
+
+function ContentBox({ title, content, isReadMode, contentTitle }: Props) {
+  const [newContent, setNewContent] = useState('');
+  const contentRef = useRef<HTMLTextAreaElement | null>(null);
+  const handleContent = useDebounceInput<string>(setNewContent);
+
+  const handleContentChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      handleContent(e.target.value);
+      if (contentRef.current) {
+        contentRef.current.style.height = 'auto';
+        contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+      }
+    },
+    []
+  );
+
+  return (
+    <section className={$['content-box']}>
+      {isReadMode ? (
+        <div style={{ minHeight: '200px' }}>
+          <h2>{title}</h2>
+          <span>{content}</span>
+        </div>
+      ) : (
+        <div className={$['textarea-box']}>
+          <h2>{contentTitle}</h2>
+          <textarea
+            defaultValue={newContent}
+            placeholder="내용을 입력해주세요."
+            ref={contentRef}
+            onChange={handleContentChange}
+          />
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default memo(ContentBox);
