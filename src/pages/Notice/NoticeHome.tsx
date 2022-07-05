f317941b1d99dde53fda95a5393c760b6b82ceda
Hyuno Choi <soonitoon@gmail.com>
2022-07-05 23:38:34 +0900

    feat: Make notice page structure

---
+++ src/pages/Notice/NoticeHome.tsx
@@ -0,0 +1,9 @@
+import { PageLayout } from 'src/components/Layout';
+
+export default function NoticeHome() {
+  return (
+    <PageLayout isNeedFooter headerHeight={44}>
+      <div>I'm Notice page!!</div>
+    </PageLayout>
+  );
+}
