b590e4baae341e7a20debc2097631b5e53b9a0d4
Hyuno Choi <soonitoon@gmail.com>
2022-07-21 13:41:40 +0900

    feat : Add friend bar

---
+++ src/__mocks__/friends.ts
@@ -0,0 +1,24 @@
+const friends = [
+  {
+    imageURL:
+      'https://image.shutterstock.com/mosaic_250/2780032/1194497251/stock-photo-portrait-of-smiling-red-haired-millennial-man-looking-at-camera-sitting-in-caf-or-coffeeshop-1194497251.jpg',
+    name: '홍길동',
+  },
+  {
+    imageURL:
+      'https://image.shutterstock.com/mosaic_250/2780032/1194497251/stock-photo-portrait-of-smiling-red-haired-millennial-man-looking-at-camera-sitting-in-caf-or-coffeeshop-1194497251.jpg',
+    name: '홍길동',
+  },
+  {
+    imageURL:
+      'https://image.shutterstock.com/mosaic_250/2780032/1194497251/stock-photo-portrait-of-smiling-red-haired-millennial-man-looking-at-camera-sitting-in-caf-or-coffeeshop-1194497251.jpg',
+    name: '홍길동',
+  },
+  {
+    imageURL:
+      'https://image.shutterstock.com/mosaic_250/2780032/1194497251/stock-photo-portrait-of-smiling-red-haired-millennial-man-looking-at-camera-sitting-in-caf-or-coffeeshop-1194497251.jpg',
+    name: '홍길동',
+  },
+];
+
+export default friends;
--- src/pages/Friends/main/index.tsx
+++ src/pages/Friends/main/index.tsx
@@ -1,6 +1,8 @@
 import { PageLayout } from 'src/components/Layout';
 import { BiSearch } from 'react-icons/bi';
+import friends from 'src/__mocks__/friends';
 import $ from './style.module.scss';
+import { NavLink } from 'react-router-dom';
 
 export default function Friends() {
   return (
@@ -9,6 +11,16 @@ export default function Friends() {
         <input className={$.input} type="text" placeholder="검색" />
         <BiSearch className={$.icon} />
       </div>
+      {friends.map(({ imageURL, name }, index) => (
+        <NavLink to="" className={$['friend-bar']} key={`${name}${index}`}>
+          <img
+            className={$['profile-image']}
+            src={imageURL}
+            alt={`${name}의 프로필 사진`}
+          />
+          <span>{name}</span>
+        </NavLink>
+      ))}
     </PageLayout>
   );
 }
