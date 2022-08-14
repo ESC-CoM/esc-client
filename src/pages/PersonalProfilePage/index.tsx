948dfa4f2e8e9ed5989141953ad7b63c276db7a7
juyeong-chungbuk <happyu9986@gmail.com>
2022-08-14 19:53:06 +0900

    ui&ux : 프로필 보기 ui 변경

---
+++ src/pages/PersonalProfilePage/index.tsx
@@ -0,0 +1,50 @@
+import $ from './style.module.scss';
+import { useEffect, useState } from 'react';
+import { basicInfoMock, moreInfoMock } from './profile';
+import { IoClose } from 'react-icons/io5';
+import { memo } from 'react';
+import { BasicSection, MoreSection } from 'src/components/PersonalProfile';
+import { BasicInfoType, MoreInfoType } from 'src/types/profile';
+
+interface Props {
+  closeModal: () => void;
+}
+
+export function PersonalProfilePage({ closeModal }: Props) {
+  const [basicInfo, setBasicInfo] = useState<BasicInfoType>({
+    img: '',
+    nickName: '',
+    gender: '',
+    college: '',
+    department: '',
+    studentNum: '',
+  });
+  const [moreInfo, setMoreInfo] = useState<MoreInfoType>({
+    birthDate: '',
+    height: '',
+    weight: '',
+    mbti: '',
+    drink: 0,
+  });
+
+  useEffect(() => {
+    setBasicInfo(basicInfoMock);
+    setMoreInfo(moreInfoMock);
+  }, []);
+
+  return (
+    <section className={$['profile-box']}>
+      <div className={$['close-profile']} onClick={closeModal}>
+        <button className={$['close-btn']}>
+          <IoClose />
+        </button>
+      </div>
+      <div className={$['profile']}>
+        <BasicSection basicInfo={basicInfo} />
+        <MoreSection moreInfo={moreInfo} />
+      </div>
+    </section>
+  );
+}
+
+export default memo(PersonalProfilePage);
