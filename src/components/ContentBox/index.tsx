90fb8887bde51fad9d253075d1d6eada5aee3dd9
juyeong-chungbuk <happyu9986@gmail.com>
2022-08-20 16:41:52 +0900

    refactor : useAutoHeightChange 반영

--- src/components/ContentBox/index.tsx
+++ src/components/ContentBox/index.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, memo, useCallback, useRef, useState } from 'react';
 import useDebounceInput from 'src/hooks/useDebounceInput';
 import $ from './style.module.scss';
+import useAutoHeightChange from 'src/hooks/useAutoHeightChange';
 
 interface Props {
   title: string;
@@ -17,11 +18,7 @@ function ContentBox({ title, content, isReadMode, contentTitle }: Props) {
   const handleContentChange = useCallback(
     (e: ChangeEvent<HTMLTextAreaElement>) => {
       handleContent(e.target.value);
-      const textareaContent = contentRef.current;
-      if (textareaContent) {
-        textareaContent.style.height = 'auto';
-        textareaContent.style.height = `${textareaContent.scrollHeight}px`;
-      }
+      useAutoHeightChange(contentRef);
     },
     []
   );
