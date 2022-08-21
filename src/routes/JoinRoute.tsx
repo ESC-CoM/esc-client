6e6819c096785aa31c128f2a27438fb01f9571aa
juyeong-chungbuk <happyu9986@gmail.com>
2022-08-22 00:29:15 +0900

    feat : Add progress bar

--- src/routes/JoinRoute.tsx
+++ src/routes/JoinRoute.tsx
@@ -5,16 +5,45 @@ import {
   MoreInfoPage1,
   MoreInfoPage2,
 } from '../pages/Join';
+import ProgressBar from 'src/components/ProgressBar';
+import { getCurrentPath } from 'src/utils/getCurrentPath';
+
+// TODO: 회원가입 환영 라우트 추가
+const routes = [
+  {
+    index: true,
+    element: <Navigate to="/join/phone" />,
+  },
+  {
+    path: 'phone',
+    element: <PhoneAuthPage />,
+  },
+  {
+    path: 'email',
+    element: <EmailInputPage />,
+  },
+  {
+    path: 'more1',
+    element: <MoreInfoPage1 />,
+  },
+  {
+    path: 'more2',
+    element: <MoreInfoPage2 />,
+  },
+];
 
 function JoinRoute() {
+  const currStep = getCurrentPath(routes);
+
   return (
-    <Routes>
-      <Route index element={<Navigate to="/join/basic/phone" />} />
-      <Route path="basic/phone" element={<PhoneAuthPage />} />
-      <Route path="basic/email" element={<EmailInputPage />} />
-      <Route path="more1" element={<MoreInfoPage1 />} />
-      <Route path="more2" element={<MoreInfoPage2 />} />
-    </Routes>
+    <>
+      <ProgressBar currStep={currStep} allStep={routes.length} />
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} {...route} />
+        ))}
+      </Routes>
+    </>
   );
 }
 
