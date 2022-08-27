e3277025098196d9b0182319f6fc140473bd598d
Bruney <drdragon777@naver.com>
2022-08-27 15:25:11 +0900

    [Feat] 홈 과팅보드 플로팅 버튼 및 사라지는 이벤트 및 애니메이션 (#122)
    * feat: add alias styles & main.scss
    
    * feat: add floating button
    
    * feat: add ref props
    
    * feat: add useDetectScroll hook
    
    * feat: add throttle
    
    * feat: add floating btn position
    
    * fix: pagelayout height
    
    * chore: change name fadein, fadeout to fadedown, fadeup
    
    * chore: reflect code review

---
+++ src/hooks/useDetectScroll.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { debounce, throttle } from 'src/lib';
+
+export default function useDetectScroll(ref: React.RefObject<HTMLElement>) {
+  const [isMove, setMove] = useState(false);
+  const handleScrollMove = debounce(setMove, 700);
+  useEffect(() => {
+    const curRef = ref.current;
+    const threshold = 0;
+    let prevScroll: number;
+    if (curRef) {
+      prevScroll = curRef.scrollTop;
+    }
+
+    const isMoreThanThres = (curScroll: number) =>
+      Math.abs(curScroll - prevScroll) > threshold;
+
+    const updateScrollDir = () => {
+      const curRef = ref.current;
+      let curScroll: number;
+
+      if (curRef) {
+        curScroll = curRef.scrollTop;
+        if (isMoreThanThres(curScroll)) {
+          setMove(true);
+          handleScrollMove(false);
+          prevScroll = curScroll > 0 ? curScroll : 0;
+        }
+      }
+    };
+
+    if (curRef)
+      curRef.addEventListener('scroll', throttle(updateScrollDir, 300));
+    return () => {
+      if (curRef)
+        curRef.removeEventListener('scroll', throttle(updateScrollDir, 300));
+    };
+  }, []);
+  return isMove;
+}
