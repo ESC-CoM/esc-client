da8704839307efa0a422cbbccde875d7e4741d22
Hyuno Choi <soonitoon@gmail.com>
2022-09-01 21:57:15 +0900

    refactor: Make friendsList template

--- src/components/shared/Search/index.tsx
+++ src/components/shared/Search/index.tsx
@@ -1,11 +1,40 @@
+import { ChangeEventHandler, KeyboardEventHandler, useState } from 'react';
 import { HiOutlineSearch } from 'react-icons/hi';
 import $ from './style.module.scss';
 
-export default function Search() {
+type Props = {
+  onSearchClick: (text: string) => void;
+};
+
+export default function Search({ onSearchClick }: Props) {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchTextChange: ChangeEventHandler<HTMLInputElement> = ({
+    target: { value },
+  }) => setSearchText(value);
+
+  const handleEnterKeyDown: KeyboardEventHandler<HTMLInputElement> = ({
+    key,
+  }) => {
+    if (key === 'Enter') onSearchClick(searchText);
+  };
+
   return (
     <div className={$['search-box']}>
-      <input type="search" placeholder="검색" />
-      <HiOutlineSearch />
+      <input
+        type="search"
+        onKeyDown={handleEnterKeyDown}
+        placeholder="검색"
+        value={searchText}
+        onChange={handleSearchTextChange}
+      />
+      <button
+        type="button"
+        aria-label="검색하기"
+        onClick={() => onSearchClick(searchText)}
+      >
+        <HiOutlineSearch />
+      </button>
     </div>
   );
 }
--- src/pages/Friends/index.ts
+++ src/pages/Friends/index.ts
@@ -1,4 +1,4 @@
-export { default as Friends } from './main';
+export { default as Friends } from './Main';
 export { default as FriendsDetail } from './detail';
 export { default as DeleteFriends } from './Delete';
 export { default as AddFriends } from './Add';
