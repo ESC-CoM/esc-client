5430747ef220ed01f8b09c7151b2ec16aa4df859
동동이 <drdragon777@naver.com>
2022-09-24 23:46:33 +0900

    refactor - refactoring profilecard component

---
+++ src/components/Meeting/MeetingDetail/ProfileCardList/ProfileBasicInfo/index.tsx
@@ -0,0 +1,32 @@
+import { IoMdFemale } from '@react-icons/all-files/io/IoMdFemale';
+import { IoMdMale } from '@react-icons/all-files/io/IoMdMale';
+import $ from './style.module.scss';
+import cx from 'classnames';
+
+type Props = {
+  friend: Pick<res.Profile, 'mannerScore' | 'nickName' | 'gender'>;
+};
+
+function ProfileCard({ friend }: Props) {
+  const { mannerScore, nickName, gender } = friend;
+
+  return (
+    <div className={$['basic-info']}>
+      <span className={$['text']}>{nickName}</span>
+      <span className={cx($['gender'], $['text'])}>
+        {gender === '남' ? (
+          <IoMdMale className={$['gender-male']} />
+        ) : (
+          <IoMdFemale className={$['gender-female']} />
+        )}
+      </span>
+      {mannerScore && (
+        <span className={cx($['manner-score'], $['text'])}>
+          {mannerScore}점
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default ProfileCard;
--- src/components/Meeting/MeetingDetail/ProfileCardList/ProfileCard/index.tsx
+++ src/components/Meeting/MeetingDetail/ProfileCardList/ProfileCard/index.tsx
@@ -1,17 +1,14 @@
 import { memo, useRef } from 'react';
-import { IoMdFemale } from '@react-icons/all-files/io/IoMdFemale';
-import { IoMdMale } from '@react-icons/all-files/io/IoMdMale';
 import { useIntersectObserver } from 'src/hooks';
 import ProfileTagContainer from '../ProfileTagContainer';
 import $ from './style.module.scss';
-import cx from 'classnames';
+import ProfileBasicInfo from '../ProfileBasicInfo';
 
 type Props = {
   friend: res.Profile;
-  profileWidth: number | undefined;
 };
 
-function ProfileCard({ friend, profileWidth }: Props) {
+function ProfileCard({ friend }: Props) {
   const { img, mannerScore, nickName, gender, birthDate } = friend;
   const { college, department, studentNum } = friend;
   const { height, weight, mbti, hobbies, drink } = friend;
@@ -48,37 +45,23 @@ function ProfileCard({ friend, profileWidth }: Props) {
   );
 
   return (
-    <div className={$['slide-view']}>
-      <div
-        className={$['profile-card']}
-        style={{ width: `${profileWidth}px` }}
-        ref={profileRef}
-      >
-        <div className={$['img-wrapper']}>
-          <img data-src={img} alt="사진" ref={imgRef} />
-        </div>
-
-        <section className={$['profile-card-container']}>
-          <div className={$['basic-info']}>
-            <span className={$['text']}>{nickName}</span>
-            <span className={cx($['gender'], $['text'])}>
-              {gender === '남' ? (
-                <IoMdMale className={$['gender-male']} />
-              ) : (
-                <IoMdFemale className={$['gender-female']} />
-              )}
-            </span>
-            {mannerScore && (
-              <span className={cx($['manner-score'], $['text'])}>
-                {mannerScore}점
-              </span>
-            )}
-          </div>
-
-          <ProfileTagContainer title="자기소개" info={info} fontSize="13px" />
-          {hobbies && <ProfileTagContainer title="취미소개" info={hobbies} />}
-        </section>
+    <div className={$['profile-card']} ref={profileRef}>
+      <div className={$['img-wrapper']}>
+        <img data-src={img} alt="사진" ref={imgRef} />
       </div>
+
+      <section className={$['profile-card-container']}>
+        <ProfileBasicInfo friend={{ nickName, gender, mannerScore }} />
+
+        <ProfileTagContainer title="자기소개" info={info} fontSize="13px" />
+        {hobbies && (
+          <ProfileTagContainer
+            title="취미소개"
+            info={hobbies}
+            fontSize="13px"
+          />
+        )}
+      </section>
     </div>
   );
 }
