0cec719351d7899831f4b96ce7048da5188b15db
Hyuno Choi <soonitoon@gmail.com>
2022-09-24 16:07:12 +0900

    refactor: Refactoring textarea resizing logic

--- src/components/Chat/MessageInput/index.tsx
+++ src/components/Chat/MessageInput/index.tsx
@@ -1,7 +1,8 @@
 import { ChangeEvent, memo, useCallback, useRef, useState } from 'react';
-import $ from './style.module.scss';
-import { IoSend, IoImages } from 'react-icons/io5';
+import { IoImages, IoSend } from 'react-icons/io5';
 import autosizeTextArea from 'src/utils/autosizeTextArea';
+
+import $ from './style.module.scss';
 
 interface Props {
   setAlbums: React.Dispatch<React.SetStateAction<FileList | null | undefined>>;
@@ -10,23 +11,18 @@ interface Props {
 export function MessageInput({ setAlbums }: Props) {
   const [newContent, setNewContent] = useState('');
   const contentRef = useRef<HTMLTextAreaElement>(null);
-  const parentRef = useRef<HTMLDivElement>(null);
 
-  const handleContentChange = useCallback((e) => {
-    const value = e.target?.value;
-    setNewContent(value);
-
-    const DivElement = parentRef.current;
-    const textareaElement = contentRef.current;
-    if (DivElement && textareaElement && textareaElement?.scrollHeight <= 96) {
-      const scrollHeight = autosizeTextArea(contentRef.current);
-
-      if (scrollHeight) {
-        DivElement.style.height = '0px';
-        DivElement.style.height = `${scrollHeight + 26}px`;
-      }
-    }
-  }, []);
+  const handleContentChange = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLTextAreaElement>) => {
+      setNewContent(value);
+      const textareaElement = contentRef.current;
+      if (!textareaElement) return;
+      const isMaxHeight = textareaElement.scrollHeight > 96;
+      if (isMaxHeight) return;
+      autosizeTextArea(textareaElement);
+    },
+    []
+  );
 
   const loadAlbum = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.currentTarget.files);
@@ -34,23 +30,19 @@ export function MessageInput({ setAlbums }: Props) {
   };
 
   return (
-    <div className={$['message-input-box']} ref={parentRef}>
+    <div className={$['message-input-box']}>
       <label htmlFor="file">
         <IoImages className={$['img-btn']} />
       </label>
       <input type="file" name="file" id="file" multiple onChange={loadAlbum} />
-
-      <div className={$['textarea-wrapper']}>
-        <textarea
-          name="message"
-          id=""
-          defaultValue={newContent}
-          placeholder="메세지 보내기..."
-          ref={contentRef}
-          onChange={handleContentChange}
-          autoFocus
-        />
-      </div>
+      <textarea
+        name="message"
+        defaultValue={newContent}
+        placeholder="메세지 보내기..."
+        ref={contentRef}
+        onChange={handleContentChange}
+        autoFocus
+      />
       <button type="submit">
         <IoSend />
       </button>
--- src/utils/autosizeTextArea.ts
+++ src/utils/autosizeTextArea.ts
@@ -1,9 +1,8 @@
 const autosizeTextArea = (textAreaRef: HTMLTextAreaElement | null) => {
   if (textAreaRef) {
     textAreaRef.style.height = '0px';
-    const scrollHeight = textAreaRef.scrollHeight + 4;
+    const scrollHeight = textAreaRef.scrollHeight;
     textAreaRef.style.height = scrollHeight + 'px';
-    return scrollHeight;
   }
 };
 
