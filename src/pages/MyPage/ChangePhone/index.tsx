d13ccc9f7a188d88f0fdd5a1de6fdc1be983b4b7
Hyuno Choi <soonitoon@gmail.com>
2022-09-26 00:06:06 +0900

    feat: Markup change/phone page

--- src/pages/MyPage/ChangePhone/index.tsx
+++ src/pages/MyPage/ChangePhone/index.tsx
@@ -1,4 +1,5 @@
 import InputWithButton from 'src/components/shared/InputWithButton';
+import InputWithTimer from 'src/components/shared/InputWithTimer';
 import { PageLayout } from 'src/components/shared/Layout';
 
 import $ from './style.module.scss';
@@ -7,10 +8,21 @@ export default function ChangePhone() {
   return (
     <PageLayout isNeedFooter={false} headerHeight={44}>
       <div className={$.container}>
+        <p className={$.discription}>
+          전화번호 변경을 위해
+          <br /> 새로운 전화번호를 인증해주세요.
+        </p>
         <InputWithButton
           onClick={() => console.log('clicked')}
           labelText="휴대폰 번호"
           buttonText="인증번호 받기"
+        />
+        <InputWithTimer
+          className={$['auth-number-input']}
+          proptype="controlled"
+          value=""
+          onChange={() => null}
+          label="인증번호"
         />
       </div>
     </PageLayout>
