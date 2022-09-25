a78ba1562278ff93066baf20cb55813f279c92a0
Hyuno Choi <soonitoon@gmail.com>
2022-09-25 14:35:32 +0900

    feat: Make basic structure of changing password page

---
+++ src/pages/MyPage/ChangePassword/index.tsx
@@ -0,0 +1,9 @@
+import { PageLayout } from 'src/components/shared/Layout';
+
+export default function ChangePassword() {
+  return (
+    <PageLayout isNeedFooter={false} headerHeight={44}>
+      비밀번호 변경 페이지
+    </PageLayout>
+  );
+}
--- src/routes/MyPageRouter.tsx
+++ src/routes/MyPageRouter.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { Authentication, Home, Select } from 'src/pages/MyPage';
+import { Authentication, ChangePassword, Home, Select } from 'src/pages/MyPage';
 
 function MyPageRouter() {
   return (
@@ -7,6 +7,7 @@ function MyPageRouter() {
       <Route index element={<Home />} />
       <Route path="authentication" element={<Authentication />} />
       <Route path="select" element={<Select />} />
+      <Route path="change/password" element={<ChangePassword />} />
     </Routes>
   );
 }
