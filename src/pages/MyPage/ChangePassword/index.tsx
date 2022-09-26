8e616795231c283bca735bce6d152e271010e2bd
Hyuno Choi <soonitoon@gmail.com>
2022-09-26 20:43:13 +0900

    feat: Use React-Hook-Form and yup for form controll

--- src/pages/MyPage/ChangePassword/index.tsx
+++ src/pages/MyPage/ChangePassword/index.tsx
@@ -2,35 +2,57 @@ import FooterButton from 'src/components/shared/FooterButton';
 import { PageLayout } from 'src/components/shared/Layout';
 import PasswordInput from 'src/components/shared/PasswordInput';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import $ from './style.module.scss';
 
+import schema from './yup';
+
+type FormData = {
+  oldPassword: string;
+  newPassword: string;
+  newPasswordConfirm: string;
+};
+
 export default function ChangePassword() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <PageLayout isNeedFooter={false} headerHeight={44}>
-      <div className={$.container}>
+      <form className={$.container} onSubmit={onSubmit}>
         <PasswordInput
           className={$['password-input']}
-          proptype="controlled"
-          value=""
-          onChange={() => null}
+          id="old-password"
+          proptype="register"
+          errorMessage={errors.oldPassword?.message}
+          register={register('oldPassword')}
           label="기존 비밀번호"
         />
         <PasswordInput
           className={$['password-input']}
-          proptype="controlled"
-          value=""
-          onChange={() => null}
+          id="new-password"
+          proptype="register"
+          errorMessage={errors.newPassword?.message}
+          register={register('newPassword')}
           label="새 비밀번호"
         />
         <PasswordInput
           className={$['password-input']}
-          proptype="controlled"
-          value=""
-          onChange={() => null}
+          id="new-password-confirm"
+          proptype="register"
+          errorMessage={errors.newPasswordConfirm?.message}
+          register={register('newPasswordConfirm')}
           label="새 비밀번호 확인"
         />
-      </div>
-      <FooterButton text="변경하기" type="button" />
+        <FooterButton text="변경하기" type="submit" />
+      </form>
     </PageLayout>
   );
 }
