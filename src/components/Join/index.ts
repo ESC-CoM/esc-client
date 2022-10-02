4e08a3a925fa7cc56a21bcb2f4550add21d77aef
Hyuno Choi <soonitoon@gmail.com>
2022-10-03 01:20:17 +0900

    chore: Remove unnecessary files

--- src/components/Join/index.ts
+++ src/components/Join/index.ts
@@ -1,7 +1,6 @@
 export { default as PhoneAuth } from './BasicInfo/PhoneAuth';
 export { default as Drink } from './MoreInfo/Drink';
 export { default as GenderInput } from './MoreInfo/GenderInput';
-export { default as NicknameInput } from './MoreInfo/NicknameInput';
 export { default as WeightInput } from './MoreInfo/WeightInput';
 export { default as ProgressBar } from './ProgressBar';
 export { default as Term } from './Term';
