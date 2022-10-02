247ab171757acf9d0439f4ced4e282c0b3d7929d
Hyuno Choi <soonitoon@gmail.com>
2022-10-02 21:52:49 +0900

    feat: Add nickname input and birth year input

--- src/pages/MyPage/ChangeAdditionalInfo/index.tsx
+++ src/pages/MyPage/ChangeAdditionalInfo/index.tsx
@@ -1,3 +1,5 @@
+import Input from 'src/components/shared/Input';
+import InputWithButton from 'src/components/shared/InputWithButton';
 import { PageLayout } from 'src/components/shared/Layout';
 
 import $ from './style.module.scss';
@@ -5,7 +7,26 @@ import $ from './style.module.scss';
 export default function ChangeAdditionalInfo() {
   return (
     <PageLayout isNeedFooter={false} headerHeight={44}>
-      <div className={$.container}>추가 정보 수정 페이지</div>
+      <div className={$.container}>
+        <InputWithButton
+          type="text"
+          proptype="controlled"
+          onClick={() => console.log('clicked')}
+          onChange={() => console.log('changed')}
+          label="별명"
+          buttonText="중복 확인"
+          placeholder="최소 2자, 최대 10자"
+          value=""
+        />
+        <Input
+          type="number"
+          proptype="controlled"
+          onChange={() => console.log('changed')}
+          label="태어난 년도"
+          placeholder="년도(4자)"
+          value=""
+        />
+      </div>
     </PageLayout>
   );
 }
