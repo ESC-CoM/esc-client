8a112c37b5a9a29dc7da252fc59a3a2e33f5d049
juyeong-chungbuk <happyu9986@gmail.com>
2022-10-09 17:05:48 +0900

    feat : 회원가입 헤더 설정

--- src/pages/Join/ProfileImageUpload/index.tsx
+++ src/pages/Join/ProfileImageUpload/index.tsx
@@ -1,11 +1,16 @@
 import React, { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from 'src/components/shared/Button';
+import FooterButton from 'src/components/shared/FooterButton';
 import { PageLayout } from 'src/components/shared/Layout';
 import PersonalProfileImage from 'src/components/shared/PersonalProfileImage';
 
 import $ from './style.module.scss';
 
+const NEXT_PATH = '/join/welcome';
+
 export default function ProfileImageUploadPage() {
+  const navigate = useNavigate();
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [profileImg, setProfileImg] = useState('');
 
@@ -21,9 +26,14 @@ export default function ProfileImageUploadPage() {
     fileInputRef?.current?.click();
   };
 
+  const handleOnSubmit = () => {
+    navigate(NEXT_PATH);
+  };
+
   return (
-    <PageLayout isNeedFooter={false} headerHeight={44}>
+    <PageLayout isNeedFooter={false} headerHeight={44} decreaseHeight={54}>
       <section className={$.container}>
+        <h1>프로필 사진을 설정해주세요</h1>
         <PersonalProfileImage
           alt=""
           src={profileImg}
@@ -41,10 +51,15 @@ export default function ProfileImageUploadPage() {
           />
           <Button
             width="100px"
-            contentText="이미지 첨부"
+            contentText="사진 업로드"
             onClick={handleClickButton}
           />
         </label>
+        <FooterButton
+          text="다음"
+          type="submit"
+          onClick={() => handleOnSubmit()}
+        />
       </section>
     </PageLayout>
   );
--- src/pages/Join/moreInfoPage1.tsx
+++ src/pages/Join/moreInfoPage1.tsx
@@ -43,7 +43,7 @@ export default function MoreInfoPage1() {
     setValue('isDuplicationChecked', true);
 
   return (
-    <PageLayout isNeedFooter={false} decreaseHeight={54}>
+    <PageLayout isNeedFooter={false} headerHeight={44} decreaseHeight={54}>
       <section className={$.container}>
         <h1>추가 정보</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
