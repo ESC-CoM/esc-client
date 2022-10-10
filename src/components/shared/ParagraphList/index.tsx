d982ba7e15fec42487ed48e6b2551f2f35625d0e
juyeong-chungbuk <happyu9986@gmail.com>
2022-10-10 22:17:07 +0900

    chore : 컴포넌트 스타일 수정

--- src/components/shared/ParagraphList/index.tsx
+++ src/components/shared/ParagraphList/index.tsx
@@ -2,13 +2,14 @@ import $ from './style.module.scss';
 
 interface Props {
   contents: string[];
+  fontSize?: number;
 }
 
-export default function ParagraphList({ contents }: Props) {
+export default function ParagraphList({ contents, fontSize }: Props) {
   return (
     <div className={$['paragraph-list']}>
       {contents.map((content, index) => (
-        <p key={content + index} className={$.content}>
+        <p key={content + index} className={$.content} style={{ fontSize }}>
           {content}
         </p>
       ))}
