fa10a4dfb002ea36da7e854255dc53f609097e9e
동동이 <drdragon777@naver.com>
2022-10-23 03:43:56 +0900

    feat - add redirect to ?kind=myfriends

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { friendMocks } from 'src/__mocks__/friendMocks';
 import { FriendsList } from 'src/components/shared/Templates';
 import { useSearch } from 'src/hooks';
@@ -6,8 +8,15 @@ import { useFriendsList, useFriendsRequests } from 'src/hooks/api/friend';
 export default function FriendsListPage() {
   const path = location.pathname;
   const kind = useSearch('kind');
+  const navigate = useNavigate();
   const { data: friendsList } = useFriendsList();
   const { data: friendsRequests } = useFriendsRequests();
+
+  useEffect(() => {
+    if (!kind) {
+      navigate('/friends/list?kind=myfriends');
+    }
+  }, []);
 
   return (
     <>
