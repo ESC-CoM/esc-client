586fe12340a7b89a0f9649c8cddad83d757b2bfc
동동이 <drdragon777@naver.com>
2022-10-23 03:19:00 +0900

    feat - add ChildrenCategory component

--- src/components/shared/Layout/Header/FriendsTab/index.tsx
+++ src/components/shared/Layout/Header/FriendsTab/index.tsx
@@ -1,8 +1,4 @@
-import cx from 'classnames';
-import { NavLink } from 'react-router-dom';
-import { useSearch } from 'src/hooks';
-
-import $ from './style.module.scss';
+import ChildrenCategory from '../ChildrenCategory';
 
 const category = [
   { name: '친구리스트', path: 'myfriends', to: '/friends/list?kind=myfriends' },
@@ -10,22 +6,5 @@ const category = [
 ];
 
 export default function FriendsTab() {
-  const keyword = useSearch('kind');
-
-  return (
-    <ul className={$['nav-list']}>
-      {category.map(({ name, path, to }, index) => (
-        <li
-          key={`nav-item-${index}`}
-          className={cx($['nav-item'], {
-            [$['item-active']]: keyword?.match(path),
-          })}
-        >
-          <NavLink to={to}>
-            <span className={$['path-name']}>{name}</span>
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  );
+  return <ChildrenCategory target="kind" {...{ category }} />;
 }
