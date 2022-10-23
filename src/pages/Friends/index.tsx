b77b67c37d88f6f8472d7244eb356f2c05c9eb70
동동이 <drdragon777@naver.com>
2022-10-23 22:13:14 +0900

    refactor - friendList rendering to conditional rendering

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -5,12 +5,17 @@ import { FriendsList } from 'src/components/shared/Templates';
 import { useSearch } from 'src/hooks';
 import { useFriendsList, useFriendsRequests } from 'src/hooks/api/friend';
 
+export type Kind = 'myfriends' | 'request' | 'myrequest';
+function useFriendData(kind: string | null) {
+  if (kind === 'myfriends') return useFriendsList();
+  return useFriendsRequests();
+}
+
 export default function FriendsListPage() {
   const path = location.pathname;
   const kind = useSearch('kind');
   const navigate = useNavigate();
-  const { data: friendsList } = useFriendsList();
-  const { data: friendsRequests } = useFriendsRequests();
+  const { data } = useFriendData(kind);
 
   useEffect(() => {
     if (!kind) {
@@ -18,23 +23,22 @@ export default function FriendsListPage() {
     }
   }, []);
 
-  return (
-    <>
-      {path === '/friends/list' && kind === 'myfriends' && (
-        <FriendsList
-          friends={friendMocks}
-          onSearchClick={(text) => {
-            console.log(text);
-          }}
-        />
-      )}
-      {path === '/friends/list' && kind === 'request' && (
-        <FriendsList
-          type="request"
-          friends={friendMocks}
-          onSearchClick={(text) => console.log(text)}
-        />
-      )}
-    </>
-  );
+  if (path === '/friends/list' && kind === 'myfriends')
+    return (
+      <FriendsList
+        friends={friendMocks}
+        onSearchClick={(text) => {
+          console.log(text);
+        }}
+      />
+    );
+  if (path === '/friends/list' && kind === 'request')
+    return (
+      <FriendsList
+        type="request"
+        friends={friendMocks}
+        onSearchClick={(text) => console.log(text)}
+      />
+    );
+  return null;
 }
