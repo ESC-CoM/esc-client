9ec13d3bf103bcddbb0239c23b2ab64e6eeca2d7
Hyuno Choi <soonitoon@gmail.com>
2022-10-23 21:36:46 +0900

    refactor: Make studentCardUpload template

--- src/components/shared/Templates/index.ts
+++ src/components/shared/Templates/index.ts
@@ -1,2 +1,3 @@
 export { default as FriendsList } from './FriendsList';
 export { default as MeetingApplyTemplate } from './MeetingApplyTemplate';
+export { default as StudentCardUpload } from './StudentCardUpload';
