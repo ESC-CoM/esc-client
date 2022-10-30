2f5cc3f236367a7b2693d46a72b46d0f466858c3
HyunoChoi <soonitoon@gmail.com>
2022-10-30 13:15:33 +0900

    [Feat] 내 정보 페이지에 로그인한 유저 정보를 불러오는 API 연동 (#208)
    ## 💡 이슈
    resolve #200
    
    ## 🤩 개요
    - 내 정보 페이지의 '기본 정보' 카드에 현재 로그인한 유저의 실제 정보를 표시하도록 API를 연동했습니다.
    
    ## 🧑‍💻 작업 사항
    - API 관련 res, req 타입 작성.
    - axios 함수 및 커스텀 쿼리 훅 작성.
    - 내 정보 페이지에 해당 API 연동.
    
    ## 📖 참고 사항
    - `api/userInfo` 도메인에 요청을 보내면 CORS 에러가 떠서 임시로 로컬 환경에 프록시 설정을 하여 작업 및 테스트
    하였습니다.
    - API 호출의 순서는 우선 현재 로그인한 유저의 토큰을 사용하여 유저 id 값을 얻어온 다음, 해당 id 값으로 유저 정보를
    받아오도록 구성하였습니다. API 호출의 의존 관계는 `useQuery`의 `enabled` 옵션을 사용하여 처리했습니다.
    - 스크린샷은 유저 DB에 있는 id 19번 유저로 로그인해 찍었습니다.
    
    <img width="386" alt="image"
    src="https://user-images.githubusercontent.com/71015915/197345186-f3851c22-39f0-4a3a-b808-1d6cf2333067.png">

--- src/pages/MyPage/Home.tsx
+++ src/pages/MyPage/Home.tsx
@@ -1,11 +1,33 @@
 import { CardBox } from 'src/components/MyPage';
+import { useGetUserValidationId } from 'src/hooks/api/auth';
+import { useGetUserDetailInformation } from 'src/hooks/api/user';
 
 import { PageLayout } from '../../components/shared/Layout';
 
 export default function Home() {
+  const {
+    data: userId,
+    isLoading: isUserIdLoading,
+    isError: isUserIdError,
+  } = useGetUserValidationId();
+
+  const {
+    data: userDetailInformation,
+    isLoading: isUserDetailInformationLoading,
+    isError: isUserDetailInformationError,
+  } = useGetUserDetailInformation(userId as number);
+
+  // TODO: 로딩, 에러 처리
+  if (isUserIdLoading) return <div>유저 식별중</div>;
+  if (isUserIdError) return <div>유저 식별 실패</div>;
+  if (!userId) return <div>유저 아이디 없음</div>;
+  if (isUserDetailInformationLoading) return <div>유저 정보 불러오는중</div>;
+  if (isUserDetailInformationError) return <div>유저 정보 불러오기 실패</div>;
+  if (!userDetailInformation) return <div>유저 정보 없음</div>;
+
   return (
     <PageLayout isNeedFooter={true} headerHeight={44}>
-      <CardBox />
+      <CardBox userInfo={userDetailInformation} />
     </PageLayout>
   );
 }
