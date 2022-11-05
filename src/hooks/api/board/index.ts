1eac40c9810acc584a8a6424cd80295b78ea039c
Hyuno Choi <soonitoon@gmail.com>
2022-11-05 16:12:51 +0900

    feat: Change useQuery to useInfiniteQuery

--- src/hooks/api/board/index.ts
+++ src/hooks/api/board/index.ts
@@ -1,12 +1,23 @@
 import { getRequestListForMeetingRegisteredByMe } from 'src/api/board';
 import { queryKey } from 'src/constants/queryKey';
 
-import { useCoreQuery } from '../core';
+import { useCoreInfiniteQuery } from '../core';
 
 export const useGetRequestListForMeetingRegisteredByMe = ({
   boardId,
   params,
 }: req.RequestListForMeetingRegisteredByMe) =>
-  useCoreQuery(queryKey.requestListForMeetingRegisteredByMeFunc(boardId), () =>
-    getRequestListForMeetingRegisteredByMe({ boardId, params })
+  useCoreInfiniteQuery(
+    queryKey.requestListForMeetingRegisteredByMeFunc(boardId),
+    ({ pageParam = 0 }) =>
+      getRequestListForMeetingRegisteredByMe({
+        boardId,
+        params: { ...params, page: pageParam },
+      }),
+    {
+      getNextPageParam: (response) => {
+        if ('status' in response) return;
+        response.page + 1;
+      },
+    }
   );
