b419dd41284be85accbf49b8b4ff8270897248c8
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-13 00:16:53 +0900

    feat : 채팅방 이동 navigate 추가

--- src/hooks/api/chat/index.ts
+++ src/hooks/api/chat/index.ts
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { createChatRoom } from 'src/api/chat';
 import { isAxiosError } from 'src/api/core';
 import { toastError, toastSuccess } from 'src/utils/toaster';
@@ -5,14 +6,19 @@ import { toastError, toastSuccess } from 'src/utils/toaster';
 import { useCoreMutation } from '../core';
 
 export const useCreateChatRoom = () => {
+  const navigate = useNavigate();
   return useCoreMutation(createChatRoom, {
     onSuccess: (data) => {
+      const {
+        data: { id },
+      } = data;
       const message =
         '미팅 신청을 수락했습니다. 채팅방에서 자유롭게 대화해보세요!';
       toastSuccess({ message });
+      navigate(`/chat/room/${id}`);
     },
     onError: (error) => {
-      if (isAxiosError<res.AuthError>(error) && !!error.response) {
+      if (isAxiosError<res.Error>(error) && !!error.response) {
         const message = '미팅 신청 오류입니다.\n다시 시도해주세요.';
         toastError({ message });
       }
