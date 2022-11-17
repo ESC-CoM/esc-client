8c1b48c91dea234f93a1b9e27658074126e07e7b
juyeong-chungbuk <happyu9986@gmail.com>
2022-11-18 05:04:55 +0900

    chore : 모르고 Devtool 빼고 커밋한거 돌려놓기..

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom';
 
 import Toast from './components/shared/Toast';
