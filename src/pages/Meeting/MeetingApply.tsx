89e7143b3633e04799ad75feb33f4f3b3093d625
동동이 <drdragon777@naver.com>
2022-11-18 04:33:29 +0900

    feat - separate FriendManager from MeetingApplyTemplate

--- src/pages/Meeting/MeetingApply.tsx
+++ src/pages/Meeting/MeetingApply.tsx
@@ -1,9 +1,6 @@
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { friendMocks } from 'src/__mocks__/friendMocks';
 import { MeetingApplyTemplate } from 'src/components/shared/Templates';
 import { useRequestMeetingQuery } from 'src/hooks/api/home';
-import { FriendType } from 'src/types/meeting';
 
 const NEXT_PATH = '/mymeeting?status=request';
 
@@ -11,23 +8,9 @@ export default function MeetingApplyPage() {
   const navigate = useNavigate();
   const { boardId } = useParams();
 
-  const [friendFetchData, setFriendFetchData] = useState<FriendType[]>([]);
-  const [addedFriendList, setAddedFriendList] = useState<number[]>([]);
   const title = '함께하고 싶다는 의사를 어필해주세요!';
 
-  useEffect(() => {
-    // TODO: fetch Data
-    setFriendFetchData(
-      friendMocks.map(({ profile, nickName }) => {
-        return {
-          src: profile,
-          name: nickName,
-        };
-      })
-    );
-  }, []);
-
-  const { data, mutate } = useRequestMeetingQuery();
+  const { mutate } = useRequestMeetingQuery();
 
   const handleClickBtn = (data: req.CreateMeeting) => {
     const { title, content, participants } = data;
@@ -43,11 +26,5 @@ export default function MeetingApplyPage() {
     );
   };
 
-  return (
-    <MeetingApplyTemplate
-      isApply
-      setAddedFriendList={(friendsIDs) => setAddedFriendList(friendsIDs)}
-      {...{ title, friendFetchData, addedFriendList, handleClickBtn }}
-    />
-  );
+  return <MeetingApplyTemplate isApply {...{ title, handleClickBtn }} />;
 }
