85af62086e37ba216ab94b7ba0c3e1c31e7f8148
Hyuno Choi <soonitoon@gmail.com>
2023-01-27 01:29:50 +0900

    test: Get response message by using MSW

--- src/api/board/index.ts
+++ src/api/board/index.ts
@@ -3,6 +3,6 @@ import { http } from '../core';
 export const deleteRequestByMe = async (
   id: number
 ): Promise<res.RequestByMe> => {
-  const response = await http.delete(`/board/request/${id}`);
+  const response = await http.delete(`/api/board/request/${id}`);
   return response.data;
 };
--- src/mocks/handlers/myMeetingHandlers.ts
+++ src/mocks/handlers/myMeetingHandlers.ts
@@ -26,3 +26,12 @@ export const getMeetingApplyList = rest.get(
     return res(ctx.status(200), ctx.json(applyMeetingMocks));
   }
 );
+
+export const deleteBoardRequestedByMe = rest.delete(
+  '/api/board/request/:id',
+  (req, res, ctx) => {
+    const { id } = req.params;
+    const message = `ID ${id} 신청을 취소했습니다.`;
+    return res(ctx.status(200), ctx.json({ message }));
+  }
+);
