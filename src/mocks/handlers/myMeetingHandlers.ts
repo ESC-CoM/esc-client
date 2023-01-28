e3a23259daf17b9fa1246a200aa925d4c474ff63
Hyuno Choi <soonitoon@gmail.com>
2023-01-28 20:56:24 +0900

    feat: Make MSW handler for allow or reject meeting request

--- src/mocks/handlers/myMeetingHandlers.ts
+++ src/mocks/handlers/myMeetingHandlers.ts
@@ -26,3 +26,20 @@ export const getMeetingApplyList = rest.get(
     return res(ctx.status(200), ctx.json(applyMeetingMocks));
   }
 );
+
+export const patchAllowOrRejectRequest = rest.patch(
+  '/board/request/:requestId',
+  async (req, res, ctx) => {
+    const { requestedId } = req.params;
+    const action = await req.json();
+    if (action === 'ALLOWED') {
+      const message = `ID ${requestedId} 요청을 수락했습니다.`;
+      return res(ctx.status(200), ctx.json({ message }));
+    }
+    if (action === 'REJECTED') {
+      const message = `ID ${requestedId} 요청을 거절했습니다.`;
+      return res(ctx.status(200), ctx.json({ message }));
+    }
+    return res(ctx.status(400), ctx.json({ message: '잘못된 요청입니다.' }));
+  }
+);
