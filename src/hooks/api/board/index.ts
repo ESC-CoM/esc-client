37fc5cf846c39c11166020c3c8ba62e246bc119e
동동이 <drdragon777@naver.com>
2023-01-30 22:39:26 +0900

    fix - fix react-query enabled option

--- src/hooks/api/board/index.ts
+++ src/hooks/api/board/index.ts
@@ -23,7 +23,7 @@ export const useGetRequestListForMeetingRegisteredByMe = ({
     {
       getNextPageParam: ({ pageable: { pageNumber }, last }) =>
         last ? undefined : pageNumber + 1,
-      enabled: !!boardId && boardId > -1,
+      enabled: boardId > -1,
     }
   );
 
