1e9ae15ccb37fbdd90ac214a05f8a930b1be42e4
juyeong-chungbuk <happyu9986@gmail.com>
2023-04-26 19:38:31 +0900

    feat: msw 페이지 네이팅 로직 구현

--- src/mocks/data/pagination.ts
+++ src/mocks/data/pagination.ts
@@ -2,3 +2,20 @@ export const pagination: Partial<res.MeetingListPagination> = {
   pageNumber: 0,
   pageSize: 10,
 };
+
+export const paginating = <T>(searchParams: URLSearchParams, list: T[]) => {
+  const size = parseInt(searchParams.get('size') || '10');
+  const page = parseInt(searchParams.get('page') || '0');
+  const startIdx = page * size;
+  const lastIdx = startIdx + size;
+
+  const sliced = list.slice(startIdx, lastIdx);
+  const len = list.length;
+  const last = len <= lastIdx;
+
+  return {
+    itemList: sliced,
+    last,
+    pageable: { pageNumber: page, pageSize: size },
+  };
+};
