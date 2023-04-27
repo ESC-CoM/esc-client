6121982f4696ef221e7948c03edbd03b519c8e09
juyeong-chungbuk <happyu9986@gmail.com>
2023-04-27 19:45:57 +0900

    feat: retry 버튼 스타일링 수정

--- src/components/shared/ErrorFallback/index.tsx
+++ src/components/shared/ErrorFallback/index.tsx
@@ -1,20 +1,19 @@
-import { MdRefresh } from '@react-icons/all-files/md/MdRefresh';
-
+import Button from '../Button';
 import $ from './style.module.scss';
 
 interface Props {
   reset: () => void;
 }
 
-function ErrorFallback({ reset }: Props) {
+const ErrorFallback = ({ reset }: Props) => {
   return (
     <div className={$['error-fallback']}>
       <span className={$['error-message']}>오류가 발생했습니다.</span>
-      <button onClick={() => reset()}>
-        <MdRefresh className={$['reset-icon']} />
-      </button>
+      <Button className={$['go-to-main-btn']} onClick={() => reset()}>
+        다시 시도
+      </Button>
     </div>
   );
-}
+};
 
 export default ErrorFallback;
