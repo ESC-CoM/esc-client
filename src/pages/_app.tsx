0ba5f88e19877f3b70794aedf0bda95b13dff03f
juyeong-chungbuk <happyu9986@gmail.com>
2023-04-30 20:50:46 +0900

    feat: msw 실행 로직 및 리액트 쿼리 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
-import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AppProps } from 'next/app';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { isDevEnv } from 'src/constants/env';
+import { initMockApi } from 'src/mocks/workers';
 
 import '@styles/reset.scss';
 
@@ -15,10 +22,17 @@ export const queryClient = new QueryClient({
   },
 });
 
+if (isDevEnv) {
+  initMockApi();
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
