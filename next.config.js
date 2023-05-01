8be8715ae7df2be111ec61abe52b57e1de32cdd3
juyeong-chungbuk <happyu9986@gmail.com>
2023-05-01 18:39:46 +0900

    chore: Disabling eslint for require

--- next.config.js
+++ next.config.js
@@ -1,18 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   webpack(config) {
-    const prod = process.env.NODE_ENV === "production";
+    const prod = process.env.NODE_ENV === 'production';
     return {
       ...config,
-      mode: prod ? "production" : "development",
-      devtool: prod ? "hidden-source-map" : "eval",
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
     };
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+      {
+        source: '/join',
+        destination: '/join/phone',
+        permanent: true,
+      },
+    ];
   },
 };
 
