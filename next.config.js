6276593a39c6620e4d5b252314abc68b9af40f5d
juyeong-chungbuk <happyu9986@gmail.com>
2023-05-02 18:30:48 +0900

    setting: 이미지 도메인 설정 및 필요없는 파일 삭제

--- next.config.js
+++ next.config.js
@@ -29,6 +29,33 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    domains: [
+      'cuser-images.githubusercontent.com',
+      'user-images.githubusercontent.com',
+      'cphoto.asiae.co.kr',
+      'media.bunjang.co.kr',
+      'www.fnnews.com',
+      'cdn.ksilbo.co.kr',
+      'cdn.ggilbo.com',
+      'search.pstatic.net',
+      'img.mbn.co.kr',
+      'news.nateimg.co.kr',
+      'encrypted-tbn0.gstatic.com',
+      'img.freepik.com',
+      'images.unsplash.com',
+      'media.istockphoto.com',
+      'dontgetserious.com',
+      'p4.wallpaperbetter.com',
+      'i.pinimg.com',
+      'images.all-free-download.com',
+      'vistapointe.net',
+      'hips.hearstapps.com',
+      'www.thegoodmorningpics.com',
+      'www.goodmorningimagesdownload.com',
+      'fiverr-res.cloudinary.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export { default as adjustDrink } from './adjustDrink';
 export { default as getQueryStringObj } from './getQueryStringObj';
-export { default as getUrlFullName } from './getUrlFullName';
 export { default as insertAutoHyphen } from './insertAutoHyphen';
 export { default as readFileAsURL } from './readFileAsURL';
